d8007ca4ea150e1a4c60ad49debccb6a78ca321e
anthonyv4 <113156175+anthonyv4@users.noreply.github.com>
2022-09-15 20:34:08 -0400

    Add files via upload

---
+++ Class 1 Homework (5 lines of code).py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+import math
+
+
+# In[5]:
+
+
+math.pow(3,4)
+
+
+# In[9]:
+
+
+np.repeat(3,5)
+
+
+# In[15]:
+
+
+np.random.randint(2,size=5)
+
+
+# In[16]:
+
+
+math.floor(7)
+
+
+# In[17]:
+
+
+math.fabs(-4)
+
+
+# In[20]:
+
+
+import numpy
+
+
+# In[24]:
+
+
+arr1=numpy.arange(4)
+
+
+# In[25]:
+
+
+print('Elements of an array1:\n',arr1)
+
+
+# In[ ]:
+
+
+
+
